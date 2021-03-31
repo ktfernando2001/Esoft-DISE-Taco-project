12ea801c78cbb15698fd0e40095046c8a3a4ec14
KTF <KTF@LAPTOP-39FEN43N>
2021-03-31 18:22:40 +0530

    DISE ESoft - Fernando 9791

---
+++ src/Taco/DBConnection.java
@@ -0,0 +1,27 @@
+package Taco; // This is you package or folder name
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class DBConnection {
+ public static final String url="jdbc:mysql://localhost:3306/taco";
+ public static final String user="root";
+ public static final String password="123";
+ static Connection con=null;
+
+ public static Connection getDatabaseConnection(){
+ try{
+ Class.forName("com.mysql.jdbc.Driver");
+ con = DriverManager.getConnection(url, user, password);
+ }catch(ClassNotFoundException | SQLException ex){
+ JOptionPane.showMessageDialog(null, "ERROR"+ex , "Error" , JOptionPane.ERROR_MESSAGE);
+ System.exit(0);
+ }
+ return con;
+ }
+}
+
+
+
